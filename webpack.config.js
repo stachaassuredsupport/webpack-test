6744c85b75a3cbd347446eb31942032192baacf4
Stacha <stacha@assuredsupport.com.au>
2023-03-20 08:50:43 +1100

    webpack cofig file set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+const path = require ('path')
+
 module.exports = {
-    mode: 'development',module
+    mode: 'development', 
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle-custom-name.js',
+    },
 }
