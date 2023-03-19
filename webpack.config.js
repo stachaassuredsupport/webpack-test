fa3b7a3739710f27f6c6f8f4943013be390e0d5b
Stacha <stacha@assuredsupport.com.au>
2023-03-20 10:54:54 +1100

    main js file removed and build script changed output to the custom named file bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,6 @@ module.exports = {
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle-custom-name.js',
+        filename: 'bundle.js',
     },
 }
