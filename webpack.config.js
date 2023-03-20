4a877919513073da11f692c1c7f141fd20176dc7
Stacha <stacha@assuredsupport.com.au>
2023-03-20 12:24:54 +1100

    bundle name changes to custom in the output part

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,13 @@ const path = require ('path')
 
 module.exports = {
     mode: 'development', 
-    entry: path.resolve(__dirname, 'src/index.js'),
+    entry: {
+       bundle_custom: path.resolve(__dirname, 'src/index.js'),
+       //there can be multiple entry sources
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+         //[name] is reffering to the name in the entry part. Right now it is "bundle_custom" see line 6" 
+        filename: '[name].js',
     },
 }
